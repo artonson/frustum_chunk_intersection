9f10bc8433375af09ec1e5fe6c8c3777715c8bc4
Alexey Artemov <artonson@yandex.ru>
2022-09-29 19:58:13 +0200

    associate_view_to_rooms.sbatch.sh

---
+++ scripts/associate_view_to_rooms.sbatch.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#SBATCH --job-name=associate-views
+#SBATCH --output=%A.out
+#SBATCH --error=%A.err
+#SBATCH --array=1-2
+#SBATCH --time=00:10:00
+#SBATCH --partition=submit
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=2g
+
+__usage="
+Usage: $0 -d data_dir [-v] <INPUT_FILE
+
+  -d: 	input directory
+  -v:   if set, verbose mode is activated (more output from the script generally)
+"
+
+usage() { echo "$__usage" >&2; }
+
+# Get all the required options and set the necessary variables
+set -x
+VERBOSE=false
+while getopts "vd:i:" opt
+do
+    case ${opt} in
+        d) DATA_DIR=$OPTARG;;
+        i) INPUT_FILENAME=$OPTARG;;
+        v) VERBOSE=true;;
+        *) usage; exit 1 ;;
+    esac
+done
+
+if [[ "${VERBOSE}" = true ]]; then
+    set -x
+    VERBOSE_ARG="--verbose"
+fi
+
+count=0
+while IFS=' ' read -r scene room type; do
+    (( count++ ))
+    if (( count == SLURM_ARRAY_TASK_ID )); then
+        break
+    fi
+done <"${INPUT_FILENAME:-/dev/stdin}"
+
+# conda init bash
+source /rhome/aartemov/miniconda3/bin/activate /rhome/aartemov/miniconda3/envs/py38_dev
+
+REPO=/rhome/aartemov/repos/frustum_chunk_intersection
+SCRIPT="${REPO}/scripts/associate_views_to_rooms.py"
+
+$SCRIPT \
+  $VERBOSE_ARG \
+  --data-dir "${DATA_DIR}" \
+  --scene "${scene}" \
+  --room "${room}" \
+  --sdf-thr 1.0 \
+  --output-dir "${DATA_DIR}"/association
